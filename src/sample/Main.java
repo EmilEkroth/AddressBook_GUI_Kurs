c4b2880e7777d90fb4c57f944cc8f13bd9fb740b
joel olausson <joel.olaus@gmail.com>
2020-03-27 10:44:59 +0100

    menupics

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,11 +14,21 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 500, 600));
+        //primaryStage.getIcons().add(new Image("Addressbook_GUI_Kurs/resources/frameIcon32.gif"));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            public void run() {
+                //Model.getInstance().shutDown();
+            }
+        }));
     }
+
+
 }
