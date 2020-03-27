8865ecada31bab6ff619641a802651d982b8ea36
joel olausson <joel.olaus@gmail.com>
2020-03-27 09:53:22 +0100

    sdasd

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.TextField;
 
@@ -10,7 +12,11 @@ public class Controller {
     Person currentPerson = new Person(0); //TODO fix list
     List<Person> people = new ArrayList<Person>(1);
 
-    public List<Button> personButton; //TODO fix list
+
+    @FXML private Button newButton;
+    @FXML private Button deleteButton;
+    @FXML private ListView personList;
+    //public List<Button> personbutton; //TODO fix list
     public TextField fieldFirst;
     public TextField fieldLast;
     public TextField fieldPhone;
@@ -18,7 +24,7 @@ public class Controller {
     public TextField fieldPost;
     public TextField fieldCity;
 
-    public void handleUpdate ()
+    public void handleUpdate()
     { //This function is called upon when a text field is edited.
         currentPerson.setFirstName(fieldFirst.getText());
         currentPerson.setLastName(fieldLast.getText());
@@ -27,12 +33,12 @@ public class Controller {
         currentPerson.setPostCode(fieldPost.getText());
         currentPerson.setCity(fieldCity.getText());
 
-        personButton.get(currentPerson.nr).setText(currentPerson.getFirstName());
+        //personbutton.get(currentPerson.nr).setText(currentPerson.getFirstName());
     }
 
     public void handleNewPerson ()
     {
             people.add(new Person(people.size()));
-            personButton.add(new Button());
+            //personbutton.add(new Button());
     }
 }
