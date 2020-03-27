0d6f1c8cfb7ed4c3a2653544b1c2cf278c29d144
Emil <35796281+ThePussyCat@users.noreply.github.com>
2020-03-27 17:01:28 +0100

    t

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -52,8 +52,6 @@ public class Controller {
                 e.printStackTrace();
             }
         loadPersons();
-
-        handleNewPerson();
         currentPerson=people.get(0);
     }
     public void handleUpdate()
@@ -148,6 +146,7 @@ public class Controller {
             }
 
         } catch (Exception ex) {
+            handleNewPerson();
             ex.printStackTrace();
         }
     }
